9c26f1f5c173a56802012c8d156f790e7fa71752
falbas <farhanalbas30@gmail.com>
2022-10-16 22:22:43 +0700

    add input form

--- main.js
+++ main.js
@@ -4,46 +4,63 @@ function putPixel(x, y) {
 }
 
 function ddaLineDrawing(x1, y1, x2, y2) {
-  let dx = Math.abs(x2 - x1)
-  let dy = Math.abs(y2 - y1)
-  let step = Math.max(dx, dy)
-  let xinc = dx/step
-  let yinc = dy/step
+  let dx = x2 - x1
+  let dy = y2 - y1
+  let step = Math.max(Math.abs(dx), Math.abs(dy))
+  let xinc = dx / step
+  let yinc = dy / step
 
-  for (let i=0; i<=step; i++) {
+  for (let i = 0; i <= step; i++) {
+    console.log(x1 + ' ' + y1)
     putPixel(Math.round(x1), Math.round(y1))
     x1 += xinc
     y1 += yinc
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const eDisplay = document.getElementById('display')
-
+function clearDisplay() {
   for (let i = 20; i >= 0; i--) {
     for (let j = 0; j <= 20; j++) {
-      const ePixel = document.createElement('div')
-      ePixel.setAttribute('id', `pixel-${j}-${i}`)
-      ePixel.classList.add('pixel')
-      eDisplay.append(ePixel)
+      ePixel = document.getElementById(`pixel-${j}-${i}`)
+      ePixel.style.backgroundColor = 'white'
     }
   }
+}
 
-  const yAxis = document.getElementById('y-axis')
-  for (let i = 20; i >= 0; i--) {
-    const yNumer = document.createElement('div')
-    yNumer.innerText = i
-    yNumer.classList.add('y-number')
-    yAxis.append(yNumer)
+const eDisplay = document.getElementById('display')
+
+for (let i = 20; i >= 0; i--) {
+  for (let j = 0; j <= 20; j++) {
+    const ePixel = document.createElement('div')
+    ePixel.setAttribute('id', `pixel-${j}-${i}`)
+    ePixel.classList.add('pixel')
+    eDisplay.append(ePixel)
   }
+}
 
-  const xAxis = document.getElementById('x-axis')
-  for (let i = 0; i <= 20; i++) {
-    const xNumer = document.createElement('div')
-    xNumer.innerText = i
-    xNumer.classList.add('x-number')
-    xAxis.append(xNumer)
-  }
+const yAxis = document.getElementById('y-axis')
+for (let i = 20; i >= 0; i--) {
+  const yNumer = document.createElement('div')
+  yNumer.innerText = i
+  yNumer.classList.add('y-number')
+  yAxis.append(yNumer)
+}
 
-  ddaLineDrawing(1, 2, 8, 6)
+const xAxis = document.getElementById('x-axis')
+for (let i = 0; i <= 20; i++) {
+  const xNumer = document.createElement('div')
+  xNumer.innerText = i
+  xNumer.classList.add('x-number')
+  xAxis.append(xNumer)
+}
+
+const inputForm = document.getElementById('input-form')
+inputForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  clearDisplay()
+  const x1 = Number(document.getElementById('x1').value)
+  const y1 = Number(document.getElementById('y1').value)
+  const x2 = Number(document.getElementById('x2').value)
+  const y2 = Number(document.getElementById('y2').value)
+  ddaLineDrawing(x1, y1, x2, y2)
 })
